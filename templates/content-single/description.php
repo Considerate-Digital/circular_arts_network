88ec367229947ca2ee3bfda18e392b80e2704b91
Alex McCartney <alexmccartney@protonmail.com>
2025-12-03 09:50:42 +0000

    fixes escaping issues in templates

--- templates/content-single/description.php
+++ templates/content-single/description.php
@@ -3,14 +3,14 @@
   if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 <div class="can-section">
-	<h2><?php echo get_the_title( $listing_id ) ?></h2>
+	<h2><?php echo esc_html(get_the_title( $listing_id )) ?></h2>
 	<div class="listing-content">
         <?php
             $content_property = get_post($listing_id);
             $content = $content_property->post_content;
             $content = apply_filters('the_content', $content);
             $content = str_replace(']]>', ']]&gt;', $content);
-            echo $content;
+            echo wp_kses_post($content);
         ?>
 	</div>
 </div>
